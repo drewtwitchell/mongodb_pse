25d8e7a899d7afe8ff0b6e25232d06f57cff0da2
Drew Twitchell <19315286+drewtwitchell@users.noreply.github.com>
2025-02-21 17:10:56 -0500

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -18,7 +18,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_mongo" {
   security_group_id = aws_security_group.terrafrom_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 27017
