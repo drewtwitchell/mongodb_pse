fa711b2cea2347add7dcacd5791f4084b28e2fb1
Drew Twitchell <19315286+drewtwitchell@users.noreply.github.com>
2025-02-21 17:09:45 -0500

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -18,6 +18,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.terrafrom_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 27017
+  ip_protocol       = "tcp"
+  to_port           = 27017
+}
+
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.terrafrom_sg.id
